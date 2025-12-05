72b4f05ae75339dc157a8d1e377f7dbe47557a92
Leandro Martins Fialho <leandrofialho2000@gmail.com>
2025-12-05 17:22:35 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,83 +1,32 @@
 ﻿using System;
-using System.Text;
-using System.Threading;
 
-namespace MaquinaCafe
+namespace Ficha8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.UTF8;
-            Random rand = new Random();
-            MaquinaCafe maquina1 = new MaquinaCafe(rand.Next(250,1500), rand.Next(50, 500), rand.Next(300, 1500), rand.Next(25, 250), 0);
-            string op;
+            int num = 0, contaentre=0, contafora=0;
 
-            do
+            Random rnd = new Random();
+
+            for (int i = 1; i <= 10; i++)
             {
-                op = maquina1.menuOpcoes();
-                switch (op.ToLower())
+                num=rnd.Next(1, 40);
+                Console.WriteLine(num);
+                if(num>=10 && num <= 20)
                 {
-                    case "1":
-                    case "expresso":
-                        if (maquina1.fazerExpresso() == true)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            Console.WriteLine("Um expresso a sair quentinho...");
-                            Console.WriteLine("Aqui está a sua bebida. Desfrute!");
-                            Console.ForegroundColor = ConsoleColor.White;
-                            maquina1.estadoMaquina();
-                        }
-                        Console.WriteLine();
-                        break;
-                    case "2":
-                    case "leite":
-                        if (maquina1.fazerLeite() == true)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            Console.WriteLine("Um leitinho a sair quentinho...");
-                            Console.WriteLine("Aqui está a sua bebida. Desfrute!");
-                            Console.ForegroundColor = ConsoleColor.White;
-                            maquina1.estadoMaquina();
-                        }
-                        Console.WriteLine();
-                        break;
-                    case "3":
-                    case "cappuccino":
-                        if (maquina1.fazerCappuccino() == true)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            Console.WriteLine("Um cappuccino a sair quentinho...");
-                            Console.WriteLine("Aqui está a sua bebida. Desfrute!");
-                            Console.ForegroundColor = ConsoleColor.White;
-                            maquina1.estadoMaquina();
-                        }
-                        Console.WriteLine();
-                        break;
-                    case "estado":
-                        maquina1.estadoMaquina();
-                        Console.WriteLine("Prima tecla para continuar...");
-                        Console.ReadKey();
-                        break;
-                    case "off":
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine("Máquina a ser desligada...");
-                        Console.ForegroundColor = ConsoleColor.White;
-                        break;
-                    default:
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Opção inválida...Tente novamente");
-                        Console.ForegroundColor = ConsoleColor.White;
-                        break;
+                    contaentre++;
                 }
+                else
+                {
+                    contafora++;
+                }
+            }
 
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Aguarde 10 segundos antes de efetuar novo pedido");
-                Console.ForegroundColor = ConsoleColor.White;
-                Thread.Sleep(10000);
-                Console.Clear();
+            Console.WriteLine("\nExistem " + contaentre + " entre 10 e 20 e " + contafora + " fora dos limites");
 
-            } while (op.ToLower() != "off");
+            Console.ReadKey();
 
         }
     }
---
+++ teste.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodos
+{
+    public class teste
+    {
+
+        public static double div(int num1, int num2)//com retorno, com parâmetros
+        {
+            double res = 0;
+            res = (double)num1 / num2;
+            return res;
+        }
+
+
+        public static void soma(int num2)//sem retorno, com parÂmetros
+        {
+            int soma = 0;
+            soma = multi() + num2;
+            Console.WriteLine("A soma é " + soma);
+        }
+
+        static void cinel()//sem retorno, sem parÂmetros
+        {
+            Console.WriteLine("O CINEL é fixe");
+        }
+
+
+        public static void soma(int num1, int num2, int num3)
+        {
+            int soma = num1 + num2 + num3;
+            Console.WriteLine("A soma é " + soma);
+        }
+
+        public static void soma(int num1, int num2, int num3, String nome)
+        {
+            int soma = num1 + num2 + num3;
+            Console.WriteLine("A soma é " + soma + " e o nome é " + nome);
+        }
+
+        public static int multi()//com retorno, sem parâmtros
+        {
+            int x = 0, y = 0, res = 0;
+            Console.WriteLine("Introduza o valor de x");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduza o valor de y");
+            y = int.Parse(Console.ReadLine());
+
+            res = x * y;
+
+            return res;
+        }
+    }
+}
